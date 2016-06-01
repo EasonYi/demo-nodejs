8acaaef4de17cec6972890b08cb3e6e9a887f6bd
Eason Yi <EasonYi@users.noreply.github.com>
2016-06-01 15:55:17 +0800

    Create myFirstHTTPServer.js

---
+++ myFirstHTTPServer.js
@@ -0,0 +1,73 @@
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+const PORT=8080; 
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+//Lets require/import the HTTP module
+var http = require('http');
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+var dispatcher = require('httpdispatcher');
+
+//Lets use our dispatcher
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('resources');
+
+//A sample GET request    
+dispatcher.onGet("/page1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});    
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
+
+//> npm install httpdispatcher
+//> node myFirstHTTPServer.js
+//GET /page1 => 'Page One'
+//POST /page2 => 'Page Two'
+//GET /page3 => 404
+//GET /resources/images-that-exists.png => Image resource
+//GET /resources/images-that-does-not-exists.png => 404
